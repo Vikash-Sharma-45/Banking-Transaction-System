9d1f91b91adad5a2ddfc3cd16f34e6b461c98f94
Vikash-Sharma-45 <vikashsharmavk58@gmail.com>
2026-04-07 22:17:16 +0530

    Register and Login Api Made

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -27,9 +27,9 @@ async function userRegisterController(req, res) {
         name
     })
 
-    const token = jwt.sign({userId: user._id}, process.env.JWT_SECRET_KEY, {expireIn : "3d"})
+    const token = jwt.sign({userId: user._id}, process.env.JWT_SECRET_KEY, {expiresIn : "3d"})
 
-    res.cookies("token", token)
+    res.cookie("token", token)
     res.send(201).json({
         user: {
             _id : user._id,
@@ -40,6 +40,46 @@ async function userRegisterController(req, res) {
     })
 }
 
+/**
+ * - user login controller
+ * - POST /api/auth/login
+ */
+
+async function userLoginController(req, res) {
+        const {email, password} = req.body;
+
+        const user = await userModel.findOne({email}).select("+password")
+
+        if(!user) {
+            return res.status(401).json({
+                message : "Email or Password is INVALID!"
+            })
+        }
+
+        const isValidPassword = await user.comparePassword(password)
+
+        if(!isValidPassword) {
+            return res.status(401).json({
+                message : "Email or Password is INVALID!"
+            })
+        }
+
+        const token = jwt.sign({userId: user._id}, process.env.JWT_SECRET_KEY, {expiresIn : "3d"})
+
+        res.cookie("token", token)
+
+        res.status(200).json({
+            user : {
+                _id : user._id,
+                email : user.email,
+                name : user.name
+            },
+            token
+        })
+}
+
+
 module.exports = {
-    userRegisterController
+    userRegisterController,
+    userLoginController
 }
